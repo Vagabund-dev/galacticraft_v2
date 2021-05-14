1ee26a6510afd307db20f91c40c6738326a11961
erozabicka <unconfigured@null.spigotmc.org>
2021-05-14 19:31:34 +0200

    menu-beta

---
+++ src/main/java/net/mcreator/lulw/procedures/MenuteleportmesicProcedure.java
@@ -0,0 +1,53 @@
+package net.mcreator.lulw.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.network.play.server.SPlayerAbilitiesPacket;
+import net.minecraft.network.play.server.SPlaySoundEventPacket;
+import net.minecraft.network.play.server.SPlayEntityEffectPacket;
+import net.minecraft.network.play.server.SChangeGameStatePacket;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.lulw.LulwModElements;
+import net.mcreator.lulw.LulwMod;
+
+import java.util.Map;
+
+@LulwModElements.ModElement.Tag
+public class MenuteleportmesicProcedure extends LulwModElements.ModElement {
+	public MenuteleportmesicProcedure(LulwModElements instance) {
+		super(instance, 11);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				LulwMod.LOGGER.warn("Failed to load dependency entity for procedure Menuteleportmesic!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		{
+			Entity _ent = entity;
+			if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+				RegistryKey<World> destinationType = RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("lulw:planet_moon"));
+				ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+				if (nextWorld != null) {
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241768_e_, 0));
+					((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), nextWorld.getSpawnPoint().getY() + 1,
+							nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+					for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+						((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+					}
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SPlaySoundEventPacket(1032, BlockPos.ZERO, 0, false));
+				}
+			}
+		}
+	}
+}
