341ec19b5e506bd63a175b089eb98a823ce3e613
erozabicka <unconfigured@null.spigotmc.org>
2021-05-18 19:59:25 +0200

    rubber_tree , sapling texture need upgrade

---
+++ src/main/java/net/mcreator/lulw/block/RubbertreewoodBlock.java
@@ -0,0 +1,48 @@
+
+package net.mcreator.lulw.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.lulw.itemgroup.SpaceModItemGroup;
+import net.mcreator.lulw.LulwModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@LulwModElements.ModElement.Tag
+public class RubbertreewoodBlock extends LulwModElements.ModElement {
+	@ObjectHolder("lulw:rubbertreewood")
+	public static final Block block = null;
+	public RubbertreewoodBlock(LulwModElements instance) {
+		super(instance, 18);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(SpaceModItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("rubbertreewood");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
